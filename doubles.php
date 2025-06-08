90f32eb98ec384d37114361e627ff3a28af138c7
Niki <nlikar@tvz.hr>
2025-06-08 18:47:45 +0200

    promijena vezana uz paginaciju

--- doubles.php
+++ doubles.php
@@ -117,26 +117,6 @@
     </table>
   </div>
 
-  <nav aria-label="Navigacija stranica">
-    <ul class="pagination justify-content-center">
-      <li class="page-item">
-        <a class="page-link" href="#" aria-label="Prethodna">
-          <span aria-hidden="true">&laquo;</span>
-          <span class="sr-only">Prethodna</span>
-        </a>
-      </li>
-      <li class="page-item"><a class="page-link" href="#">1</a></li>
-      <li class="page-item"><a class="page-link" href="#">2</a></li>
-      <li class="page-item"><a class="page-link" href="#">3</a></li>
-      <li class="page-item">
-        <a class="page-link" href="#" aria-label="Sljedeća">
-          <span aria-hidden="true">&raquo;</span>
-          <span class="sr-only">Sljedeća</span>
-        </a>
-      </li>
-    </ul>
-  </nav>
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
